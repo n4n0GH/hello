5f4ba12a617fba224b03327e3276061e69cd9094
n4n0GH <n4n0@tuta.io>
2019-11-16 23:37:53 +0100

    removed build directory

--- kwin-effects/qt5build/CMakeFiles/kwin4_effect_helloshaders_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kwin4_effect_helloshaders_autogen"
-  "kwin4_effect_helloshaders_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kwin4_effect_helloshaders_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
